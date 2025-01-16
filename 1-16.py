ff57cf599415b24fe46cfbead79b4d87bcab4b90
Frosmin <simonabastomart@gmail.com>
2025-01-16 19:17:37 -0400

    Añadir función de punto fijo y cálculo de errores para mejorar la convergencia

--- 1-16.py
+++ 1-16.py
@@ -3,4 +3,18 @@ import numpy as np
 
 fx = lambda x: -np.log(2-x)
 
-print(fx(1.5))
+error = lambda x2, x1: abs((x2-x1)/x2)
+
+def punto_fijo(fx, x0, t):
+    i=0
+    x1 = fx(x0)
+    while error(x1,x0) >t:
+        i+=1
+        x0 = x1
+        x1 = fx(x0)
+    print(i)
+    return x1
+
+
+
+print(punto_fijo(fx, 1.5, 0.0000000001))
