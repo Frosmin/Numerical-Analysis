1b2d153be7b8ab72c4bca39c3454cea410aece05
Frosmin <simonabastomart@gmail.com>
2025-01-29 23:40:03 -0400

    Refactor la implementación del método de Simpson en dos direcciones; simplificar funciones y mejorar la legibilidad del código.

--- segundo parcial/simpson_dosDireciones.py
+++ segundo parcial/simpson_dosDireciones.py
@@ -1,12 +1,5 @@
 import numpy as np
 
-fx = lambda x, y: x*((x**2) + y)
-
-def f (x,y):
-    return x*((x**2) + y)
-
-
-
 def y(fx,hy,a,b,x,n):
     
     res = 0
@@ -15,75 +8,66 @@ def y(fx,hy,a,b,x,n):
         if i == 0:
             p = 1
             res += p * fx(x,a)
-            #print(p * fx(x,a))
             i += 1
             a += hy
         elif a == b:
             p = 1
             res += p * fx(x, a)
-            #print(p * fx(x,a))
             i += 1
             a += hy
         elif i % 2 != 0:
             p = 4
             res += p * fx(x, a)
-            #print(p * fx(x,a))
             i += 1
             a += hy
         else:
             p = 2
             res += p * fx(x, a)
-            #print(p * fx(x,a))
             i += 1
             a += hy
     return (hy/3)*res 
 
-
-
 def x(fx,hx,hy,a,b,c,d,n):
     res = 0
     i = 0
-    while i < n+1:
+    while i < n+1: 
         if i == 0:
             p = 1
-            res += p * y(fx,hy,a,b,c,n)
-            #print(p * y(fx,hy,a,b,c,n))
+            res += p * y(fx,hy,c,d,a,n)
             i += 1
-            c += hx
-        elif c == b:
+            a += hx
+        elif a == b:
             p = 1
-            res += p * y(fx,hy,a,b,c,n)
-            #print(p * y(fx,hy,a,b,c,n))
+            res += p * y(fx,hy,c,d,a,n)
             i += 1
-            c += hx
+            a += hx
         elif i % 2 != 0:
             p = 4
-            res += p * y(fx,hy,a,b,c,n)
-            #print(p * y(fx,hy,a,b,c,n))
+            res += p * y(fx,hy,c,d,a,n)
             i += 1
-            c += hx
+            a += hx
         else:
             p = 2
-            res += p * y(fx,hy,a,b,c,n)
-            #print(p * y(fx,hy,a,b,c,n))
+            res += p * y(fx,hy,c,d,a,n)
             i += 1
-            c += hx
+            a += hx
     return (hx/3)*res
 
 
-hx = 0.5
-hy = 0.5    
+
+
+def simpson(fx,a,b,c,d,n):
+    hx = (b-a)/n
+    hy = (d-c)/n
+    print(x(fx,hx,hy,a,b,c,d,n))
+
+
+
 a = 1
 b = 3
 c = 2
 d = 4
 n = 4
+fx = lambda x, y: x*((x**2) + y)
 
-
-print (x(fx,hx,hy,a,b,c,d,n))
-
-
-# print(fx(3,4))
-
-
-#print (fx(2.5,4))
+simpson(fx,a,b,c,d,n)
