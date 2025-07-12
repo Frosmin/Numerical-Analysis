1a2d077435938d93780dd60303ddcb8b719bf071
Frosmin <simonabastomart@gmail.com>
2025-07-11 22:19:11 -0600

    Refactor iteration logic in iteraciones.py for clarity and consistency

--- samuel/iteraciones.py
+++ samuel/iteraciones.py
@@ -7,41 +7,45 @@ n = 3.9
 x = 0.2
 valor_verdadero = xi(n)
 
+primero = x
+segundo = ecuacion1(primero, n)
+
 resultado = x
 print("ejer 1 ---------------------------------------------------------------------------------------------")
-while valor_verdadero != resultado:
-    resultado = ecuacion1(resultado, n) 
-    print(resultado)
+while primero != segundo:
+    primero = segundo
+    segundo = ecuacion1(segundo, n) 
+    print(segundo)
 
-print(resultado)
+print(segundo)
 
 
 
 
 ##############################################################################################################
-xi2 = lambda n: math.sqrt(n)
-ecuacion2 = lambda x, n: 0.45*(x+n/x)
+# xi2 = lambda n: math.sqrt(n)
+# ecuacion2 = lambda x, n: 0.45*(x+n/x)
 
-n2 = 3
-x2 = 2
-EP = 0.000000012 
-valor_verdadero2 = xi2(n2)
-
-
-x0 = x2
-x1 = ecuacion2(x0, n2)
+# n2 = 3
+# x2 = 2
+# EP = 0.000000012 
+# valor_verdadero2 = xi2(n2)
 
 
-resultado2 =  0
+# x0 = x2
+# x1 = ecuacion2(x0, n2)
 
 
-print("ejer 2 ---------------------------------------------------------------------------------------------")
-while abs(x0 - x1) > EP:
-    x0 = x1
-    x1 = ecuacion2(x1, n2) 
-    print(x1)
+# resultado2 =  0
 
-print(x1)
+
+# print("ejer 2 ---------------------------------------------------------------------------------------------")
+# while abs(x0 - x1) > EP:
+#     x0 = x1
+#     x1 = ecuacion2(x1, n2) 
+#     print(x1)
+
+# print(x1)
 
 
 
